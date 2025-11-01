0e415b2155db33b2b9d1e9c2ecde3abc1b7e7ea4
yvdzke . <yudzke@gmail.com>
2025-11-01 20:01:51 +0700

    Menambah icons react dan merubah homepage

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import React from "react";
 import Nav from "./components/Nav.jsx";
 import Home from "./components/Home.jsx";
 import Footer from "./components/Footer.jsx";
+import Tech from "./components/Technical.skills.jsx";
 
 import "./App.css";
 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,43 @@
 import React from "react";
 import "./styles/Footer.css";
-
+import { ImGithub } from "react-icons/im";
+import { FaLinkedin } from "react-icons/fa";
+import { BsTwitterX } from "react-icons/bs";
 function Footer() {
   return (
     <footer className="footer">
-      <p>© 2025 Yuda Pradana</p>
+      <div className="footer-container">
+        <p>© 2025 Yuda Pradana</p>
+        <ul className="social-links">
+          <li>
+            <a
+              href="https://github.com/yvdzke"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <ImGithub size={21} />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/yuda-pradana-8243182a3/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaLinkedin size={21} />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://x.com/yvdzke"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <BsTwitterX size={21} />
+            </a>
+          </li>
+        </ul>
+      </div>
     </footer>
   );
 }
